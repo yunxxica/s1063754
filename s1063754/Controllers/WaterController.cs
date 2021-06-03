63c24bc2df462f005c0d951bf238e5a4c267744f
方姝云 <minnki17@gmail.com>
2021-06-03 11:00:59 +0800

    add water

---
+++ s1063754/Controllers/WaterController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace s1063754.Controllers
+{
+    public class WaterController : Controller
+    {
+        // GET: Water
+        public ActionResult Index(string name, float? weight)
+        {
+            var result = "";
+
+
+            if (weight.HasValue)
+            {
+                result = (weight * 30) + "cc";
+            }
+
+            ViewBag.name = name;
+            ViewBag.result = result;
+
+            return View();
+        }
+    }
+}
