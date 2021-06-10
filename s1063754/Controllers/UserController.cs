339c8fe3bbf9a25b529592c7dfd9375f62db792e
方姝云 <minnki17@gmail.com>
2021-06-10 21:52:39 +0800

    0610

--- s1063754/Controllers/UserController.cs
+++ s1063754/Controllers/UserController.cs
@@ -20,23 +20,10 @@ namespace s1063754.Controllers
 
         public ActionResult SignUp(SignUpData data)
         {
-            if (data.Name.IsNullOrWhiteSpace())
-            {
-                data.NameMessage = " 請輸入姓名";
-            }
+            
 
-            if (data.Account.IsNullOrWhiteSpace())
-            {
-                data.AccountMessage = "請輸入帳號";
-            }
+            if (ModelState.IsValid) {
 
-            if (data.Password.IsNullOrWhiteSpace())
-            {
-                data.PasswordMessage = "請輸入密碼";
-            }
-
-            if (data.Name.IsNullOrWhiteSpace() == false && !data.Account.IsNullOrWhiteSpace() && !data.Password.IsNullOrWhiteSpace())
-            {
                 data.Message = "註冊成功";
             }
             
